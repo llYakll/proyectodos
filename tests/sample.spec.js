fc7a48a80518a6c1984df49cd00e8e441c1e0ffd
Gregory Oladipo <goladipo@instructors.2u.com>
2024-04-02 18:06:46 -0600

    created jest-test folder

---
+++ tests/sample.spec.js
@@ -0,0 +1,12 @@
+// import what you want to test
+
+/** describe('my beverage', () => {
+  test('is delicious', () => {
+    expect(myBeverage.delicious).toBeTruthy();
+  });
+
+  test('is not sour', () => {
+    expect(myBeverage.sour).toBeFalsy();
+  });
+});
+**/
