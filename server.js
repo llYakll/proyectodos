4847207f9a1086671e81fd2b255561509aece5bc
bcgosselin <bcgosselin@gmail.com>
2024-04-09 21:12:11 -0500

    added handlebars without logic

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -28,7 +28,8 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers })
+const hbs = exphbs.create({});
+// const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
