ce3a8ccda3d75822cc6ea729b80c1062220da69d
wacwestley30 <westleycervantes@gmail.com>
2024-04-11 00:25:31 -0400

    updated userRoutes in controllers and updated both model and controller Jest tests.

--- tests/models/user.test.js
+++ tests/models/user.test.js
@@ -13,9 +13,7 @@ describe('User model', () => {
     it('should create a new user', async () => {
         const userData = {
             username: 'test1user',
-            password: 'password1234',
-            regDate: new Date(),
-            lastLogin: new Date()
+            password: 'password1234'
         };
         const user = await User.create(userData);
         expect(user.username).toEqual('test1user');
@@ -28,9 +26,7 @@ describe('User model', () => {
     it('should hash the password before saving', async () => {
         const userData = {
             username: 'test2user',
-            password: 'password1234',
-            regDate: new Date(),
-            lastLogin: new Date()
+            password: 'password1234'
         };
         const user = await User.create(userData);
         expect(user.password).not.toEqual('password1234');
@@ -39,9 +35,7 @@ describe('User model', () => {
     // Tests so that username will not allow NULL
     it('should not create a user without a username', async () => {
         const userData = {
-            password: 'password123',
-            regDate: new Date(),
-            lastLogin: new Date()
+            password: 'password123'
         };
         let error;
         try {
@@ -56,10 +50,8 @@ describe('User model', () => {
     // Tests to fail for too short of a password
     it('should not create a user with a short password', async () => {
         const userData = {
-            username: 'testuser3',
-            password: 'pass',
-            regDate: new Date(),
-            lastLogin: new Date()
+            username: 'test3user',
+            password: 'pass'
         };
         let error;
         try {
