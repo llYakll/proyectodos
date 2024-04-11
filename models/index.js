4b1778f18bfa44a5e3e6630fbe9c87e4b10e6a2b
wacwestley30 <westleycervantes@gmail.com>
2024-04-10 22:57:12 -0400

    edited models/index.js to show the deletion of other models

--- models/index.js
+++ models/index.js
@@ -1,19 +1,7 @@
 const Card = require('./card');
 const Card_Collection = require('./card_collection');
 const Collection = require('./collection');
-const Pokemon = require('./pokemon');
-const Team = require('./team');
-const Team_Roster = require('./team_roster');
 const User = require('./user');
-
-/* User Relationships (Bottom) */
-User.hasMany(Team, {
-    foreignKey: 'userID'
-});
-
-Team.belongsTo(User, {
-    foreignKey: 'userID'
-});
 
 User.hasOne(Collection, {
     foreignKey: 'userID'
@@ -23,24 +11,6 @@ Collection.belongsTo(User, {
     foreignKey: 'userID'
 });
 
-/* Pokemon & Team Relationships (Top & Left) */
-Pokemon.hasMany(Card, {
-    foreignKey: 'pokeID'
-});
-
-Card.belongsTo(Pokemon, {
-    foreignKey: 'pokeID'
-});
-
-Pokemon.hasMany(Team_Roster, {
-    foreignKey: 'pokeID'
-});
-
-Team_Roster.belongsTo(Pokemon, {
-    foreignKey: 'pokeID'
-});
-
-/* Card & Collection Relationships (Right) */
 Card_Collection.hasMany(Card, {
     foreignKey: 'cardID'
 });
@@ -57,4 +27,4 @@ Collection.belongsTo(Card_Collection, {
     foreignKey: 'collectionID'
 });
 
-module.exports = { Card, Card_Collection, Collection, Pokemon, Team, Team_Roster, User };
+module.exports = { Card, Card_Collection, Collection, User };
