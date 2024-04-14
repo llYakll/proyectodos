fbda3105e4c1a5379f6acdd96962bbbc367cdfc0
wacwestley30 <westleycervantes@gmail.com>
2024-04-14 12:03:30 -0400

    added search functionality. Refactored search.handlebars and added axios to help create the searchRoutes.js

---
+++ controllers/searchRoutes.js
@@ -0,0 +1,93 @@
+const router = require('express').Router();
+const axios = require('axios');
+
+const subtypes = [
+    `BREAK`,
+    `Baby`,
+    `Basic`,
+    `EX`,
+    `GX`,
+    `Goldenrod Game Corner`,
+    `Item`,
+    `LEGEND`,
+    `Level-Up`,
+    `MEGA`,
+    `Pokémon Tool`,
+    `Pokémon Tool F`,
+    `Rapid Strike`,
+    `Restored`,
+    `Rocket's Secret Machine`,
+    `Single Strike`,
+    `Special`,
+    `Stadium`,
+    `Stage 1`,
+    `Stage 2`,
+    `Supporter`,
+    `TAG TEAM`,
+    `Technical Machine`,
+    `V`,
+    `VMAX`
+];
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('search', { subtypes: subtypes});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// Route for searching cards
+router.post('/', async (req, res) => {
+    try {
+        const { card_name, card_subtypes } = req.body;
+        let apiUrl = `https://api.pokemontcg.io/v2/cards?q=`;
+
+        // console.log('this is the req:', req);
+        // console.log('this is the card_name:', card_name);
+        // console.log('this is the card_subtypes:', card_subtypes);
+
+        if (card_name) {
+            apiUrl += `name:${card_name}*&`;
+        }
+
+        if (card_subtypes) {
+            apiUrl += `subtypes:"${card_subtypes}"&`;
+        }
+
+        // Remove trailing '&' if it exists
+        if (apiUrl.endsWith('&')) {
+            apiUrl = apiUrl.slice(0, -1);
+        }
+
+        console.log('API URL:', apiUrl);
+
+        // Set a timeout of 10 seconds (10000 milliseconds)
+        const config = {
+            timeout: 10000 // milliseconds
+        };
+
+        // Make request to TCG API to search for cards
+        const response = await axios.get(apiUrl, config);
+
+        // console.log('API response:', response.data);
+        
+        if (response.status !== 200) {
+            throw new Error('Error searching for cards');
+        }
+
+        // Extract card data from the API response
+        const cards = response.data.data;
+
+        // console.log('Cards:', cards);
+
+        // Render search results page with the retrieved card data and subtypes
+        res.render('search', { cards: cards, subtypes: subtypes });
+    } catch (error) {
+        console.error('Error searching cards:', error);
+        res.status(500).json({ error: 'An error occurred while searching for cards.' });
+    }
+});
+
+module.exports = router;
