23c862136e1a442b4f838b927569ff03ce8e25a6
llYakll <150628047+llYakll@users.noreply.github.com>
2024-04-20 10:13:43 -0500

    added index.js and collection routes.

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const collectionRoutes = require('./collection')
 const homeRoutes = require('./homeRoutes');
 const searchRoutes = require('./searchRoutes');
 const apiRoutes = require('./api');
@@ -7,5 +7,6 @@ const apiRoutes = require('./api');
 router.use('/', homeRoutes);
 router.use('/search', searchRoutes);
 router.use('/api', apiRoutes);
+router.use('/collection', collectionRoutes)
 
 module.exports = router;
