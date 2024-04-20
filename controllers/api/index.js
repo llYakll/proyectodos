7957cdfe3ab023edc04c0c41bb2de9762652278e
llYakll <150628047+llYakll@users.noreply.github.com>
2024-04-20 10:01:59 -0500

    replaced old code with my revised files. files changed are index.js, tcgRoutes, and userRoutes in the api sub-directory of Controllers.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,13 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
 // import routers
-// const TCGRouter = require('./TCG');
-// const PokemonRouter = require('./pokeAPI');
-// const loginRouter = require('./login');
+const TCGRouter = require('./TCG');
 const userRoutes = require('./userRoutes');
 
 // mount routers onto the parent router
-// router.use('/tcg', TCGRouter);
-// router.use('/pokemon', PokemonRouter);
-// router.use('/auth', loginRouter);
+router.use('/search', TCGRouter);
 router.use('/users', userRoutes);
 
-module.exports = router;
+
+module.exports = Router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,89 +1,26 @@
-const router = require('express').Router();
-const User = require('../../models/user');
-const bcrypt = require('bcrypt');
+const express = require('express');
+const { createUser, userLogin, logOut } = require('../../utils/userMw')
+const userRoutes = express.Router();
 
-// Route for retrieving all users for testing
-router.get('/', async (req, res) => {
+userRoutes.post('/', async (req, res) => { //i cant find the correct end point???
     try {
-        const users = await User.findAll();
-        res.status(200).json(users);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// Route for creating a new user
-router.post('/', async (req, res) => {
-	try {
-		const dbUserData = await User.create({
-			username: req.body.username,
-			password: req.body.password,
-		});
-
-		req.session.save(() => {
-			req.session.logged_in = true;
-			req.session.user_id = dbUserData.user_id;
-
-			res.status(200).json(dbUserData);
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
-});
-
-// Route for logging in
-router.post('/login', async (req, res) => {
-	try {
-		// Check if username and password are provided
-		if (!req.body.username || !req.body.password) {
-			console.log('no username or password');
-			return res.status(400).json({ message: 'Username and password are required.' });
+        const newUser = await createUser(req, res);
+        res.status(201).json({ user: newUser, message: 'User created successfully!' });
+		res.redirect('/');
+		} catch (error) {
+			console.error('Registration error:', error);
 		}
-		
-		const dbUserData = await User.findOne({
-			where: {
-				username: req.body.username,
-			},
-		});
-	
-		if (!dbUserData) {
-			return res.status(400).json({ message: 'Incorrect username or password. Please try again!' });
-		}
-
-		// console.log('this is the req.body.password:', req.body.password);
-		// console.log('this is the dbUserData.password:', dbUserData.password);
-
-		const validPassword = await bcrypt.compare(req.body.password, dbUserData.password);
-
-		console.log('this is the validPassword:', validPassword);
-	
-		if (!validPassword) {
-			return res.status(400).json({ message: 'Incorrect username or password. Please try again!' });
-		}
-
-		req.session.save(() => {
-			req.session.logged_in = true;
-			req.session.user_id = dbUserData.user_id;
-	
-			console.log('You are successfully logged in!');
-			res.status(200).json({ user: dbUserData, message: 'You are now logged in!' });
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({ message: 'An error occurred while processing your request. Please try again later.' });
-	}
 });
 
-// Route for logging out
-router.post('/logout', (req, res) => {
-	if (req.session.logged_in) {
-			req.session.destroy(() => {
-			res.status(204).end();
-		});
-	} else {
-		res.status(404).end();
-	}
+userRoutes.post('/login', userLogin, (req, res) => {
+    res.status(200).json({ message: 'You are now logged in!', user: req.user });
+	res.redirect('/');
 });
 
-module.exports = router;
+userRoutes.post('/logout', logOut, (req, res) => {
+    res.status(204).end();
+	res.redirect('/');
+});
+
+
+module.exports = userRoutes;
